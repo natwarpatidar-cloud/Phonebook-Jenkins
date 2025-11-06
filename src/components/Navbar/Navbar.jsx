856110c3f72305c5f9a8caf00e6aba101ec0b6c3
NatwarPatidar <natwar.patidar@gammaedge.io>
2025-11-06 18:25:09 +0530

    some random changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -41,7 +41,7 @@ export default function Navbar () {
             <div className="py-2 w-full flex justify-between items-center mt-4">
                 <div className="flex gap-2">
                     <CircleUserRound />
-                    <span>PhoneBook</span>
+                    <span>Lol</span>
                 </div>
 
                 <div className="border border-black/2 rounded-lg">
